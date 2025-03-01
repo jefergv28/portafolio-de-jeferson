217155654abd1103938b1a3cebbc86b639e23964
jefergv28 <galvisvegayeferson@gmail.com>
2025-02-28 19:03:35 -0500

    se agrga el cv en pdf para ser descargado y se actualiza el favicon

--- app/page.jsx
+++ app/page.jsx
@@ -32,6 +32,14 @@ const Home = () => {
                 variant="outline"
                 size="lg"
                 className="uppercase flex items-center gap-2"
+                onClick={() => {
+                  const link = document.createElement("a");
+                  link.href = "/cv.pdf";
+                  link.download = "Jeferson_Galvis_CV.pdf";
+                  document.body.appendChild(link);
+                  link.click();
+                  document.body.removeChild(link);
+                }}
               >
                 <span>Download CV</span>
                 <FiDownload className="text-xl" />
